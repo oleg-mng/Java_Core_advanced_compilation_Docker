a8509e51b14685f5ce27e6fc7eed71066a270c99
oleg <legik7127@gmail.com>
2023-05-29 22:51:00 +0600

    added archive .jar

---
+++ src/main/java/org/core/another/Decorator.java
@@ -0,0 +1,7 @@
+package org.core.another;
+
+public class Decorator {
+    public static String decorate(int a){
+        return String.format("Total number is: %d", a);
+    }
+}
--- src/main/java/org/core/some/Main.java
+++ src/main/java/org/core/some/Main.java
@@ -1,8 +1,18 @@
 package org.core.some;
 
+import org.core.another.Decorator;
+import org.core.another.Operation;
+
 public class Main {
     public static void main(String[] args) {
-
+        var res = Operation.add(2, 3);
+        System.out.println(Decorator.decorate(res));
+        res = Operation.sub(2, 3);
+        System.out.println(Decorator.decorate(res));
+        res = Operation.mul(2, 3);
+        System.out.println(Decorator.decorate(res));
+        res = Operation.div(2, 3);
+        System.out.println(Decorator.decorate(res));
 
     }
 }
