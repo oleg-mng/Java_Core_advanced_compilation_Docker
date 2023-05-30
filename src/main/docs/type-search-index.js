9300424793409ac3724b327a5f13157f5642ea1d
oleg <legik7127@gmail.com>
2023-05-30 19:00:03 +0600

    added dockerfile

---
+++ src/main/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.core.another","l":"Decorator"},{"p":"org.core.some","l":"Main"},{"p":"org.core.another","l":"Operation"}];updateSearchResults();
