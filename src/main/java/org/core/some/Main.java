e26f258408d780e6a89989eede8cc1d17113c7ac
oleg <legik7127@gmail.com>
2023-05-31 13:18:12 +0600

    added some commands in comments

--- src/main/java/org/core/some/Main.java
+++ src/main/java/org/core/some/Main.java
@@ -3,6 +3,17 @@ package org.core.some;
 import org.core.another.Decorator;
 import org.core.another.Operation;
 
+/**
+ * javac -sourcepath ./java -d out java/org/core/some/Main.java
+ *
+ * java -classpath ./out org.core.some.Main
+ *
+ * docker build . -t myappwithdocker
+ *
+ *  docker run myappwithdocker
+ *
+ */
+
 public class Main {
     public static void main(String[] args) {
         var res = Operation.add(2, 3);
